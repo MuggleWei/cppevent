83989658a52bb785c00c6b7047775a63bd97434c
Muggle Wei <mugglewei@gmail.com>
2018-02-03 10:52:44 +0800

    add comments

--- src/cppevent/core/conn.h
+++ src/cppevent/core/conn.h
@@ -44,7 +44,8 @@ public:
 	 */
 	cppevent_core_EXPORT size_t peekBytes(void *data_out, size_t datalen);
 	/*
-	 * get datalen bytes in input byte buffer and draining the bytes from the source buffer
+	 * get datalen bytes in input byte buffer and draining the bytes from 
+	 * the source buffer
 	 * @data_out: the destination byte array to store the result
 	 * @data_len: the maximum size of the destination byte array
 	 * @RETURN: the number of bytes read, or -1 represent failed
@@ -65,16 +66,42 @@ public:
 	 */
 	cppevent_core_EXPORT int write(ByteBuffer& buf);
 	/*
-	 * Move datalen number of bytes from anthoer ByteBuffer into output byte buffer of this conn
+	 * Move datalen number of bytes from anthoer ByteBuffer into output byte 
+	 * buffer of this conn
 	 * @buf: the source byte buffer
 	 * @datalen: the number of bytes want to move
 	 * @RETURN: 0 if successful, or -1 if an error occurred
 	 */
 	cppevent_core_EXPORT int write(ByteBuffer& buf, size_t datalen);
+
+	/*
+	 * write data into output byte buffer and close this conn after data write 
+	 * into tcp socket buffer
+	 * @data_out: the byte array
+	 * @datalen: the number of bytes to be copied into output byte buffer
+	 * @RETURN: 0 if successful, or -1 if an error occurred
+	 */
 	cppevent_core_EXPORT int writeAndClose(void *data_out, size_t datalen);
+	/*
+	 * Move data from anthoer ByteBuffer into output byte buffer of this 
+	 * conn and close this conn after data write into tcp socket buffer
+	 * @buf: the source byte buffer
+	 * @RETURN: 0 if successful, or -1 if an error occurred
+	 */
 	cppevent_core_EXPORT int writeAndClose(ByteBuffer& buf);
+	/*
+	 * Move datalen number of bytes from anthoer ByteBuffer into output byte 
+	 * and close this conn after data write into tcp socket buffer
+	 * buffer of this conn
+	 * @buf: the source byte buffer
+	 * @datalen: the number of bytes want to move
+	 * @RETURN: 0 if successful, or -1 if an error occurred
+	 */
 	cppevent_core_EXPORT int writeAndClose(ByteBuffer& buf, size_t datalen);
 
+	/*
+	 * util function for peek or read data from input byte buffer
+	 */
 	cppevent_core_EXPORT int16_t peekInt16();
 	cppevent_core_EXPORT int32_t peekInt32();
 	cppevent_core_EXPORT int64_t peekInt64();
@@ -87,6 +114,7 @@ public:
 	cppevent_core_EXPORT double readDouble();
 
 	/*
+	 * util function for write data to output byte buffer
 	 *  @RETURN: number of byte that write into bytebuf
 	 */
 	cppevent_core_EXPORT int writeInt16(int16_t val);
--- src/cppevent/core/event_handler.h
+++ src/cppevent/core/event_handler.h
@@ -13,14 +13,34 @@ class EventHandler
 public:
 	virtual ~EventHandler() {}
 
+	/*
+	 * When a new socket connected, trigger this function
+	 */
 	cppevent_core_EXPORT virtual void connActive(std::shared_ptr<Conn>&) {}
+
+	/*
+	 * When a socket disconnected, trigger this function
+	 */
 	cppevent_core_EXPORT virtual void connInactive(std::shared_ptr<Conn>&) {}
 
+	/*
+	 * Have data in conn's input byte buffer, trigger this function
+	 */
 	cppevent_core_EXPORT virtual void connRead(std::shared_ptr<Conn>&) {}
+
+	/*
+	 * After data write into socket buffer, trigger this function
+	 */
 	cppevent_core_EXPORT virtual void connWrite(std::shared_ptr<Conn>&) {}
 
+	/*
+	 * Some event happened
+	 */
 	cppevent_core_EXPORT virtual void connEvent(std::shared_ptr<Conn>&) {}
 
+	/*
+	 * Did not receive the message within n seconds, n is set by idleTimeout
+	 */
 	cppevent_core_EXPORT virtual void connIdleTimeout(std::shared_ptr<Conn>&) {}
 };
 
